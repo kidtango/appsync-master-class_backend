26b69ad521d5e6022c94871205cf6a223882c257
Scott Tang <scott.q.tang@gmail.com>
2023-10-13 20:38:50 -0500

    ..

--- playground.js
+++ playground.js
@@ -1,35 +1,95 @@
+const adSummary = 'ADSUMMARY'
+const XML = 'XML'
+const none = 'NONE'
+const comment = 'COMMENT'
+const rendition = 'RENDITION'
+const low = 'LOW'
+
 const items = [
   {
-    PK: 'USER|123',
-    SK: 'REGULAR',
-    userId: 'USER|123',
-    GSI: 'user',
-    __typename: 'user',
+    PK: 'QCSESSION|vast123',
+    SK: '1|ADSUMMARY|XML',
+    entityType: adSummary,
+    category: XML,
+    subCategory: none,
+    sessionId: 1,
   },
   {
-    PK: 'USER|123',
-    SK: 'TWEET|123',
-    text: 'tweet 1',
-    GSI: 'tweet',
-    entity: 'TWEET',
-    __typename: 'tweet',
-    GS2: 'TWEET|123',
+    PK: 'QCSESSION|vast123',
+    SK: '1|ADSUMMARY|RENDITION|LOW_1',
+    entityType: adSummary,
+    category: XML,
+    subCategory: none,
+    sessionId: 1,
   },
   {
-    PK: 'USER|123',
-    SK: 'TWEET|345',
-    text: 'tweet 2',
-    GSI: 'tweet',
-    __typename: 'tweet',
+    PK: 'QCSESSION|vast123',
+    SK: '1|COMMENT|XML',
+    entityType: comment,
+    category: XML,
+    subCategory: none,
+    sessionId: 1,
   },
   {
-    PK: 'USER|123',
-    SK: 'RETWEET|123',
-    GSI: 'retweet',
-    __typename: 'retweet',
-    GS2: 'TWEET|123',
+    PK: 'QCSESSION|vast123',
+    SK: '1|COMMENT|RENDITION|LOW_1',
+    entityType: comment,
+    category: rendition,
+    subCategory: low,
+    sessionId: 1,
+  },
+  {
+    PK: 'QCSESSION|vast456',
+    SK: '2|ADSUMMARY|XML',
+    entityType: adSummary,
+    category: XML,
+    subCategory: none,
+    sessionId: 2,
+  },
+  {
+    PK: 'QCSESSION|vast456',
+    SK: '2|ADSUMMARY|RENDITION|LOW_1',
+    entityType: adSummary,
+    category: XML,
+    subCategory: none,
+    sessionId: 2,
+  },
+  {
+    PK: 'QCSESSION|vast456',
+    SK: '2|COMMENT|XML',
+    entityType: comment,
+    category: XML,
+    subCategory: none,
+    sessionId: 2,
+  },
+  {
+    PK: 'QCSESSION|vast456',
+    SK: '2|COMMENT|RENDITION|LOW_1',
+    entityType: comment,
+    category: rendition,
+    subCategory: low,
+    sessionId: 2,
   },
 ]
 
-const tweets = items.filter((item) => item.__typename === 'retweet')
-console.log('🚀 ~ file: playground.js:32 ~ tweets:', tweets)
+const sessionIds = [1, 2]
+
+const organizedSessions = sessionIds.reduce((acc, sessionId) => {
+  acc[sessionId] = items.filter((item) => item.sessionId === sessionId)
+  return acc
+}, {})
+console.log(
+  '🚀 ~ file: playground.js:74 ~ organizedSessions ~ organizedSessions:',
+  organizedSessions
+)
+
+const qcResults = {
+  xml: {
+    adSummary: {},
+    comments: [],
+  },
+  reditions: [
+    { adSummary: {}, comments: [] },
+    { adSummary: {}, comments: [] },
+  ],
+}
